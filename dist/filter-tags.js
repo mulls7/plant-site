def1968f65abb4c24e194e83dc2e514b214e91a3
mulls7 <59904900+mulls7@users.noreply.github.com>
2022-12-11 20:08:57 -0500

    added notes

--- dist/filter-tags.js
+++ dist/filter-tags.js
@@ -2,61 +2,61 @@
 
 (function() {
 
-  var $imgs = $('#gallery .project-list__project');
-  var $buttons = $('#buttons #b1');
-  var tagged = {};
+  var $imgs = $('#gallery .project-list__project'); // Store all images
+  var $buttons = $('#buttons #b1'); // Store buttons element
+  var tagged = {};  // Create tagged object
 
-  $imgs.each(function() {
-    var img = this;
-    var tags = $(this).data('tags');
+  $imgs.each(function() { // Loop through images and
+    var img = this; // Store img in variable
+    var tags = $(this).data('tags'); // Get this element's tags
 
-    if (tags) {
-      tags.split(',').forEach(function(tagName) {
-        if (tagged[tagName] == null) {
-          tagged[tagName] = [];
+    if (tags) { // If the element had tags
+      tags.split(',').forEach(function(tagName) { // Split at comma and
+        if (tagged[tagName] == null) { // If object doesn't have tag
+          tagged[tagName] = []; // Add empty array to object
         }
-        tagged[tagName].push(img);
+        tagged[tagName].push(img); // Add image to the array
       });
     }
   });
   // buttons, event handlers, and filters go here
-  $('<button/>', {
-    text:'Show All',
-    class: 'active',
-    click: function() {
-      $(this)
-        .addClass('active')
-        .siblings()
-        .removeClass('active')
-        .parent()
-          .parent()
-          .children('#b2')
-          .children()
-          .removeClass('active');;
-      $imgs.show();
+  $('<button/>', { // Create empty button
+    text:'Show All', // Add text 'show all'
+    class: 'active', // Make it active
+    click: function() { // Add onclick handler to it
+      $(this) // Get the clicked on button
+        .addClass('active') // Add the class of active
+        .siblings() // Get its siblings
+        .removeClass('active') // Remove active class
+        .parent() // Select parent 
+          .parent() // Select parent
+          .children('#b2') // Select children with ID of b2
+          .children() // Select child
+          .removeClass('active'); // Remove active class from child
+      $imgs.show(); // Show all images
     }
-  }).appendTo($buttons)
+  }).appendTo($buttons) // Add to buttons
 
 
-  $.each(tagged, function(tagName) {
-    $('<button/>' , {
-      text: tagName + '(' + tagged[tagName].length + ')',
-      click: function() {
-        $(this)
-          .addClass('active')
-          .siblings()
-          .removeClass('active')
-          .parent()
-          .parent()
-          .children('#b2')
-          .children()
-          .removeClass('active');
-        $imgs
-          .hide()
-          .filter(tagged[tagName])
-          .show();
+  $.each(tagged, function(tagName) { // For each tag name
+    $('<button/>' , { // Create an empty button
+      text: tagName + '(' + tagged[tagName].length + ')', // Add tag name
+      click: function() { // Add click handler
+        $(this) // The button clicked on
+          .addClass('active') // Make clicked item active
+          .siblings() // Get its siblings
+          .removeClass('active') // Remove active from them
+          .parent() // Get parent
+          .parent() // Get parent
+          .children('#b2') // Get child with ID of b2
+          .children() // Get child
+          .removeClass('active'); // Remove class of active
+        $imgs // With all of the images
+          .hide() // Hide them
+          .filter(tagged[tagName]) //Find ones with this tag
+          .show(); // Show just those images
       }
-    }).appendTo($buttons);
+    }).appendTo($buttons); // Add to the buttons
   });
 }());
 
--- dist/photo-viewer.js
+++ dist/photo-viewer.js
@@ -1,60 +1,61 @@
-var request;
-var $current;
-var cache = {};
-var $frame = $("#photo-viewer");
-var $thumbs = $(".thumb");
+var request; // Latest image to be requested
+var $current; // Image currently being shown
+var cache = {}; // Cache object
+var $frame = $("#photo-viewer"); // Container for image
+var $thumbs = $(".thumb"); // Container for thumbnails
 
-function crossfade($img) {
-  if ($current) {
-    $current.stop().fadeOut("slow");
+function crossfade($img) { // Function to fade between images
+  // Pass in new image as parameter
+  if ($current) { // If there is currently an image showing 
+    $current.stop().fadeOut("slow"); // Stop the animation and fade it out
   }
-  $img.css({
-    marginLeft: -$img.width() / 2,
-    marginTop: -$img.height() / 2
+  $img.css({ // Set the CSS margins for the image
+    marginLeft: -$img.width() / 2, // Negative margin of half image's width
+    marginTop: -$img.height() / 2 // Negative margin of half image's height
   });
-  $img.stop().fadeTo("slow", 1);
-  $current = $img;
+  $img.stop().fadeTo("slow", 1); // Stop animation on new image & fade in 
+  $current = $img; // New image becomes current image
 }
 
-$(document).on("click", ".thumb", function (e) {
-  var $img;
-  var src = this.href;
-  request = src;
+$(document).on("click", ".thumb", function (e) { // When a thumb is clicked on
+  var $img; // Create local variable called $img
+  var src = this.href; // Store path to image
+  request = src; // Store path again in request
 
-  e.preventDefault();
+  e.preventDefault(); // Stop default link behavior
 
-  $thumbs.removeClass("thumb-active");
-  $(this).addClass("thumb-active");
+  $thumbs.removeClass("thumb-active"); // Remove active from all thumbs
+  $(this).addClass("thumb-active"); // Add active to clicked thumb
 
-  if (cache.hasOwnProperty(src)) {
-    if (cache[src].isLoading === false) {
-      crossfade(cache[src].$img);
-    }
-  } else {
-    $img = $("<img/>");
-    cache[src] = {
-      $img: $img,
-      isLoading: true
+  if (cache.hasOwnProperty(src)) { // If cache contains this image
+    if (cache[src].isLoading === false) { // And if isLoading is false
+      crossfade(cache[src].$img); // Call crossfade() function
+    } 
+  } else { // Otherwise it is not in cache
+    $img = $("<img/>"); // Store empty <img/> element in $img
+    cache[src] = { // Store this image in cache
+      $img: $img, // Add the path to the image
+      isLoading: true // Set isLoading property to true
     };
 
     //next few lines will run when image has loadied but are prepared first
-    $img.on("load", function () {
-      $img.hide();
+    $img.on("load", function () { // When image has loaded
+      $img.hide(); // Hide it
       //Remove is-loading class from frame & append new image to it
       $frame.removeClass("is-loading").append($img);
-      cache[src].isLoading = false;
+      cache[src].isLoading = false; // Update isLoading in cache
       // if still most recently requested image then
       if (request === src) {
-        crossfade($img);
+        crossfade($img); // Call crossfade() function
       }
     });
-    $frame.addClass("is-loading");
+    $frame.addClass("is-loading"); // Add is-loading class to frame
 
-    $img.attr({
-      src: src,
-      alt: this.title || ""
+    $img.attr({ // Set attributes on <img> element
+      src: src, // Add src attribute to load image
+      alt: this.title || "" // Add title if one was given in link
     });
   }
 });
 //last line runs once(when rest of script has loaded) to show the first image
-$(".thumb").eq(0).click();
+$(".thumb").eq(0).click(); // Stimulate click on first thumbnail
